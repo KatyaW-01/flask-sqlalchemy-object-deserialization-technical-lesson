63ffcbefbd541a12b5c3762e63c98487ca537e57
Katya Weicht <katya.weicht@gmail.com>
2025-07-25 14:16:28 -0600

    deserialize a collection

--- lib/deserialize.py
+++ lib/deserialize.py
@@ -21,3 +21,19 @@ hamster_json = '{"name": "Wiggles", "breed": "Siberian", "dob": "2020-01-30"}'
 result_2 = hamster_schema.loads(hamster_json)
 print(type(result_2))
 pprint(result_2)
+
+#deserialize a collection
+hamster_1 = {"name": "Nibbles", "breed": "European",  "dob": "2018-04-30"}
+hamster_2 = {"name": "Snuggles", "breed": "Chinese", "dob": "2023-10-07"}
+hamster_list = [hamster_1, hamster_2]
+result_3 = hamster_schema.load(hamster_list, many = True)
+print(type(result_3)) 
+pprint(result_3) 
+
+hamster_1_0 = '{"name": "Honey", "breed": "Turkish", "dob": "2009-06-03"}'
+hamster_2_0 = '{"name": "Squeaky", "breed": "Winter White", "dob": "2022-12-31"}'
+hamsters  = f'[{hamster_1_0}, {hamster_2_0}]'  
+hamster_schema_many = HamsterSchema(many=True)
+result_4 = hamster_schema_many.loads(hamsters)
+print(type(result_4))
+pprint(result_4)
