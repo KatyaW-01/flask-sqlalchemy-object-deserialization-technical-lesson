4203e8733a4a0d15679a4aa3f817fdb291d0af41
Katya Weicht <katya.weicht@gmail.com>
2025-07-25 14:13:05 -0600

    Deserialize with load and loads

--- lib/deserialize.py
+++ lib/deserialize.py
@@ -9,3 +9,15 @@ class HamsterSchema(Schema):
      name = fields.Str()
      breed = fields.Str()
      dob = fields.Date()
+
+hamster_schema = HamsterSchema()
+
+hamster_dict = {"name": "Fluffernutter", "breed": "Roborovski", "dob": "2014-08-11"}
+result = hamster_schema.load(hamster_dict)
+print(type(result))
+pprint(result)
+
+hamster_json = '{"name": "Wiggles", "breed": "Siberian", "dob": "2020-01-30"}'
+result_2 = hamster_schema.loads(hamster_json)
+print(type(result_2))
+pprint(result_2)
